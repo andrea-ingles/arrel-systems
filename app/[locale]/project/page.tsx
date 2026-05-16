192bf454d55ddd3a1e361c4d68a2a1bb8df15c41
Andrea Inglés <andreaingles91@gmail.com>
2026-05-16 19:24:37 +0200

    fix:again naming modules build and project pages

--- app/[locale]/project/page.tsx
+++ app/[locale]/project/page.tsx
@@ -1,7 +1,7 @@
 import { getTranslations } from 'next-intl/server'
 import type { Metadata } from 'next'
 import { locales } from '@/lib/i18n-config'
-import ProjectPageClient from './ProjectPageClient'
+import ProjectPageClient from './Projectpageclient'
 
 interface Props {
   params: { locale: string }
